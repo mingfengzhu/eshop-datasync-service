b8b0825289f31b7ce1dbce28b04b854abecc3dc0
mingfeng.zhu <yunyi0510@163.com>
2017-11-06 21:26:57 +0800

    datasync commit

---
+++ src/main/java/www/itech/eshop/datasync/service/fallback/EshopProductServiceFallback.java
@@ -0,0 +1,45 @@
+package www.itech.eshop.datasync.service.fallback;
+
+import org.springframework.stereotype.Component;
+
+import www.itech.eshop.datasync.service.EshopProductService;
+
+@Component
+public class EshopProductServiceFallback implements EshopProductService {
+
+	public String findBrandById(Long id) {
+		
+		return null;
+	}
+
+	public String findBrandByIds(String ids) {
+		
+		return null;
+	}
+
+	public String findCategoryById(Long id) {
+		
+		return null;
+	}
+
+	public String findProductIntroById(Long id) {
+		
+		return null;
+	}
+
+	public String findProductPropertyById(Long id) {
+		
+		return null;
+	}
+
+	public String findProductById(Long id) {
+		
+		return null;
+	}
+
+	public String findProductSpecificationById(Long id) {
+		
+		return null;
+	}
+
+}
